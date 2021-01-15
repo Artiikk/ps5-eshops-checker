9d3a751f476ecf82c41e42dbcc826c69534cd3ee
Artiikk <harfild1@ukr.net>
2021-01-15 20:39:33 +0200

    changed: expected fromat

--- index.js
+++ index.js
@@ -14,19 +14,19 @@ const webSitesUrl = [
     url: 'https://rozetka.com.ua/playstation_5_digital_edition_2/p223596301/',
     name: 'rozetka',
     expectedPath: 'body > app-root > div > div:nth-child(2) > app-rz-product > div > product-tab-main > div:nth-child(1) > div:nth-child(1) > div.product-about__right > product-main-info > ul > li:nth-child(1) > p',
-    expectedText: 'Немає в наявності'
+    expectedText: ['Немає в наявності', 'Нет в наличии']
   },
   {
     url: 'https://allo.ua/ru/igrovye-pristavki/konsol-playstation-5-digital-edition.html',
     name: 'allo',
     expectedPath: '#__layout > div > div.page__content.page__content--inverse > div.product-view > ul > li.product-basic-content.product-view-content__section > div.product-basic-content__data.main-data > div.main-data__trade.product-trade > div > div > p',
-    expectedText: 'Нет в наличии'
+    expectedText: ['Нет в наличии']
   },
   {
     url: 'https://www.moyo.ua/igrovaya_pristavka_playstation_5_digital_edition_pervaya_postavka_/475056.html',
     name: 'moyo',
     expectedPath: '#main-product > div.tovar-tabs > div.tovar-tabs-content-list > ul > li.tovar-tabs-content.tovar-main-tab-content.tovar-tabs-content--active > div.tovar_maininfo > div.tovar_info.tovar-status-type--archive > div.tovar-info-price.tovar-info-price--archive.Roboto.tovar-info-price--without-pre-order-start-date > div.tovar-info-price-header > div',
-    expectedText: 'Товар закончился'
+    expectedText: ['Товар закончился']
   }
 ];
 
@@ -75,7 +75,7 @@ async function urlChecker({ url, name, expectedPath, expectedText }) {
   }
 
   console.log(`${name.toUpperCase()} current: ${element} - expected: ${expectedText}`)
-  if (element !== expectedText) {
+  if (!expectedText.includes(element)) {
     sendUpdate(name, url)
   }
   return element
