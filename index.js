8cff2e79cd27ee7953554711b1c894f293b6934c
Artur Shestopalov <artiikk2@gmail.com>
2021-02-23 23:36:41 +0100

    R58845: added navigation ui fixes

--- index.js
+++ index.js
@@ -76,6 +76,14 @@ const sendMissingUpdate = (name, url) => {
   );
 };
 
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  console.log('chatId', chatId)
+
+  // send a message to the chat acknowledging receipt of their message
+  // bot.sendMessage(chatId, 'Received your message');
+});
+
 async function urlChecker({ url, name, expectedPath, expectedText }) {
   const element = await getElement(url, expectedPath)
 
